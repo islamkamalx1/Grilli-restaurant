cbaf14d3187cc2132c1a39da30cc3604467c70ca
Islam Kamal <islamkamalx1>
2022-12-18 16:05:02 +0200

    hero section with slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,3 +66,69 @@ window.addEventListener("scroll", () => {
     header.classList.remove("active");
   }
 });
+
+/**
+ * HERO SLIDER
+ */
+
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSlideItem = heroSliderItems[0];
+
+const updateSliderPos = () => {
+  lastActiveSlideItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSlideItem = heroSliderItems[currentSlidePos];
+};
+
+const slideNext = () => {
+  if (currentSlidePos >= heroSliderItems.length - 1) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  updateSliderPos();
+};
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = () => {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = heroSliderItems.length - 1;
+  } else {
+    currentSlidePos--;
+  }
+
+  updateSliderPos();
+};
+
+heroSliderNextBtn.addEventListener("click", slidePrev);
+
+/**
+ * AUTO SLIDE
+ */
+
+let autoSlideInterval;
+
+const autoSlide = () => {
+  autoSlideInterval = setInterval(() => {
+    slideNext();
+  }, 7000);
+};
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", () =>
+  clearInterval(autoSlideInterval)
+);
+
+addEventOnElements(
+  [heroSliderPrevBtn, heroSliderNextBtn],
+  "mouseout",
+  autoSlide
+);
+
+window.addEventListener("load", autoSlide);
